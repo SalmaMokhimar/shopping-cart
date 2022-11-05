1315d4398bf3d5df006f04a07d368e199170e291
salma <salma.mokhimar@gmail.com>
2022-11-05 16:04:21 +0200

    fix public images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import Input from "./Input";
 import PropTypes from "prop-types";
 import {ReactComponent as Nasnav} from '../images/nasnav.svg';
 import {ReactComponent as FooterLogo} from '../images/footerlogo.svg';
-import {ReactComponent as Sub} from '../image/sub.svg';
+import {ReactComponent as Sub} from '../images/sub.svg';
 
 class Footer extends React.Component {
     render(){
